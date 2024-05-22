85f872b27abf1cb2c2547806ea5e367f2876366b
Joao Carlos - PC Online <joao.carlos.dev.log@gmail.com>
2024-05-22 20:36:20 -0300

    Update 1-engine.bat

--- 1-engine.bat
+++ 1-engine.bat
@@ -1,6 +1,7 @@
 python -m venv .venv
 call .venv\Scripts\activate
 python -m pip install --upgrade pip
+pip install --upgrade poetry
 pip install -r requirements.txt
 echo.
 echo Tudo foi instalado com sucesso
